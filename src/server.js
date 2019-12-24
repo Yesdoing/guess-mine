46f9e2c252d966676dc6df5f1f73b6874ca221b0
Yesdoing <fkrmtleo12@gmail.com>
2019-12-24 13:36:10 +0900

    setting socket.io and gulp styles

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import { join } from 'path';
-import express from 'express';
-import socketIO from 'socket.io';
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
 import logger from "morgan";
 
 const PORT = 4000;
@@ -9,11 +9,10 @@ app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
-app.get('/', (req, res) => res.render("home"));
+app.get("/", (req, res) => res.render("home"));
 
-
-const handleListening = () => console.log(`✅ Server running: http://localhost:${PORT}`);
-
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
 
@@ -21,8 +20,15 @@ const io = socketIO.listen(server);
 
 let sockets = [];
 
-io.on("connect", (socket) => {
-  sockets.push(socket.id);
-});
+io.on("connect", socket => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotification", {
+      message,
+      nickname: socket.nickname || "Anon"
+    });
+  });
 
-setInterval(() => console.log(sockets), 1000);
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
